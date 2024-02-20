779862f32111f0aa3cb0042eec6cf1664a67ddfc
sdtczfy <sdtczfy@gmail.com>
2024-02-20 17:48:29 +0800

    IO_Pick_and_Place RLDS format

---
+++ IO_Pick_and_Place/README.md
@@ -0,0 +1,7 @@
+TODO(IO_Pick_and_Place): Markdown description of that will appear on the catalog page.
+Description is **formatted** as markdown.
+
+
+- The main camera,left camera, right camera are cropped into (360, 640) from (720,1280)
+- The depth camera is the original size of (720, 1280)
+- The fisheye is cropped into (640, 800) from (1024, 1280) 
--- visualize_dataset.py
+++ visualize_dataset.py
@@ -6,21 +6,21 @@ import argparse
 import tqdm
 import importlib
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # suppress debug warning messages
+
+os.environ["TF_CPP_MIN_LOG_LEVEL"] = "3"  # suppress debug warning messages
 
 
 WANDB_ENTITY = None
-WANDB_PROJECT = 'vis_rlds'
+WANDB_PROJECT = "vis_rlds"
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('dataset_name', help='name of the dataset to visualize')
+parser.add_argument("dataset_name", help="name of the dataset to visualize")
 args = parser.parse_args()
 
 if WANDB_ENTITY is not None:
     render_wandb = True
-    wandb.init(entity=WANDB_ENTITY,
-               project=WANDB_PROJECT)
+    wandb.init(entity=WANDB_ENTITY, project=WANDB_PROJECT)
 else:
     render_wandb = False
 
@@ -29,20 +29,19 @@ else:
 dataset_name = args.dataset_name
 print(f"Visualizing data from dataset: {dataset_name}")
 module = importlib.import_module(dataset_name)
-ds = tfds.load(dataset_name, split='train')
+ds = tfds.load(dataset_name, split="train")
 ds = ds.shuffle(100)
 
 # visualize episodes
 for i, episode in enumerate(ds.take(5)):
     images = []
-    for step in episode['steps']:
-        images.append(step['observation']['image'].numpy())
+    for step in episode["steps"]:
+        images.append(step["observation"]["image"].numpy())
     image_strip = np.concatenate(images[::4], axis=1)
-    caption = step['language_instruction'].numpy().decode() + \
-        ' (temp. downsampled 4x)'
+    caption = step["language_instruction"].numpy().decode() + " (temp. downsampled 4x)"
 
     if render_wandb:
-        wandb.log({f'image_{i}': wandb.Image(image_strip, caption=caption)})
+        wandb.log({f"image_{i}": wandb.Image(image_strip, caption=caption)})
     else:
         plt.figure()
         plt.imshow(image_strip)
@@ -51,8 +50,8 @@ for i, episode in enumerate(ds.take(5)):
 # visualize action and state statistics
 actions, states = [], []
 for episode in tqdm.tqdm(ds.take(500)):
-    for step in episode['steps']:
-        actions.append(step['action'].numpy())
+    for step in episode["steps"]:
+        actions.append(step["action"].numpy())
         # states.append(step['observation']['state'].numpy())
 actions = np.array(actions)
 # states = np.array(states)
@@ -66,9 +65,9 @@ def vis_stats(vector, vector_mean, tag):
     assert vector.shape[1] == vector_mean.shape[0]
 
     n_elems = vector.shape[1]
-    fig = plt.figure(tag, figsize=(5*n_elems, 5))
+    fig = plt.figure(tag, figsize=(5 * n_elems, 5))
     for elem in range(n_elems):
-        plt.subplot(1, n_elems, elem+1)
+        plt.subplot(1, n_elems, elem + 1)
         plt.hist(vector[:, elem], bins=20)
         plt.title(vector_mean[elem])
 
@@ -76,7 +75,7 @@ def vis_stats(vector, vector_mean, tag):
         wandb.log({tag: wandb.Image(fig)})
 
 
-vis_stats(actions, action_mean, 'action_stats')
+vis_stats(actions, action_mean, "action_stats")
 # vis_stats(states, state_mean, 'state_stats')
 
 if not render_wandb:
